54fb5e79e803b6b09724cddcd0b699a35ac60c88
Albin B Thomas <albinbthomas8800@gmail.com>
2025-12-03 22:50:37 +0530

    footer responsive added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,27 @@
 "use client";
 
-
 export default function Footer() {
-    return (
-        <div className="h-[200px] bg-white flex flex-col  py-10 mt-5  ">
-            <div className="flex justify-between mx-[180px]">
-                <p className="text-[16px] text-[#61758A]">Privacy Policy</p>
-                <p className="text-[16px] text-[#61758A]">Terms and Conditions</p>
-                <p className="text-[16px] text-[#61758A]">Shipping and Returns</p>
-            </div>
+  return (
+    <div className="bg-white py-10 mt-10 border-t border-[#E5E8EB]">
+      
+      
+      <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-4 md:gap-6 px-6 md:px-20 lg:px-40">
+        <p className="text-[16px] text-[#61758A] cursor-pointer">Privacy Policy</p>
+        <p className="text-[16px] text-[#61758A] cursor-pointer">Terms and Conditions</p>
+        <p className="text-[16px] text-[#61758A] cursor-pointer">Shipping and Returns</p>
+      </div>
 
-            <div className="flex justify-center mt-6 gap-4 items-center mx-[180px]">
-                <img className="w-6 h-6" src="/icons/twitter.svg" alt="twitter" />
-                <img className="w-6 h-6" src="/icons/instagram.svg" alt="instagram" />
-                <img className="w-6 h-6" src="/icons/facebook.svg" alt="facebook" />
-            </div>
+      
+      <div className="flex justify-center mt-6 gap-6">
+        <img className="w-6 h-6" src="/icons/twitter.svg" alt="twitter" />
+        <img className="w-6 h-6" src="/icons/instagram.svg" alt="instagram" />
+        <img className="w-6 h-6" src="/icons/facebook.svg" alt="facebook" />
+      </div>
 
-            <p className="text-center mt-6 text-[16px] text-[#61758A]">@2024 ShopSmart. All rights reserved.</p>
-        </div>
-    )
+     
+      <p className="text-center mt-6 text-[14px] md:text-[16px] text-[#61758A]">
+        ©2024 ShopSmart. All rights reserved.
+      </p>
+    </div>
+  );
 }
